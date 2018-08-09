9503da5d4e31eb7dbcae49a199b528f7eeea6338
RuslanHumeniuk <rus_gumeniuk@ukr.net>
2018-08-09 21:32:24 +0300

    SUCCESSFULL DEPLOY

---
+++ NotABookShared/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotABookShared
+{
+    class Class1
+    {
+        public string GetStr()
+        {
+            return "rest";
+        }
+    }
+}
