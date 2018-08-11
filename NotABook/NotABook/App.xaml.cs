1bad9b9cb0557d7eabcc46ca60c1263511dd7f1d
RuslanHumeniuk <rus_gumeniuk@ukr.net>
2018-08-12 00:18:03 +0300

    Created menu and toolbar in wpf

--- NotABook/NotABook/App.xaml.cs
+++ NotABook/NotABook/App.xaml.cs
@@ -32,25 +32,29 @@ namespace NotABook
         public App ()
 		{
             InitializeComponent();
-            {
-                currentBook = new Book("My first book");
-                Book secondBook = new Book("Second book");
-                Book thirdBook = new Book("Third book");
 
-                Category chocolateCategory = new Category(currentBook, "Chocolate");
-                Category flourCategory = new Category(currentBook, "Flour");
-                Category eggsCategory = new Category(currentBook, "Eggs");
-                Category potatoCategory = new Category(currentBook, "Potato");
-                Category tomatoCategory = new Category(currentBook, "Tomato");
-                Category chickenCategory = new Category(currentBook, "Chicken");
-
-                Item chocolateBiscuit = new Item(currentBook, "Chocolate biscuit", Description.CreateDescription("The best chocolate cake ever"), new ObservableCollection<Category>() { chocolateCategory, flourCategory, eggsCategory });
-                Item salatWithPotatoAndTomato = new Item(currentBook, "Salat with potat, tomatos and eggs", Description.CreateDescription("Very healthy salat"), new ObservableCollection<Category>() { potatoCategory, tomatoCategory, eggsCategory });
-                Item chicken = new Item(currentBook, "Chicken", Description.CreateDescription("Chicken like in KFC"), new ObservableCollection<Category>() { chickenCategory, eggsCategory });
-            }                      
+            SetUp();                     
 
 			MainPage = new Pages.MainPages.StartPage();                                  
 		}
+
+        private void SetUp()
+        {
+            currentBook = new Book("My first book");
+            Book secondBook = new Book("Second book");
+            Book thirdBook = new Book("Third book");
+
+            Category chocolateCategory = new Category(currentBook, "Chocolate");
+            Category flourCategory = new Category(currentBook, "Flour");
+            Category eggsCategory = new Category(currentBook, "Eggs");
+            Category potatoCategory = new Category(currentBook, "Potato");
+            Category tomatoCategory = new Category(currentBook, "Tomato");
+            Category chickenCategory = new Category(currentBook, "Chicken");
+
+            Item chocolateBiscuit = new Item(currentBook, "Chocolate biscuit", Description.CreateDescription("The best chocolate cake ever"), new ObservableCollection<Category>() { chocolateCategory, flourCategory, eggsCategory });
+            Item salatWithPotatoAndTomato = new Item(currentBook, "Salat with potat, tomatos and eggs", Description.CreateDescription("Very healthy salat"), new ObservableCollection<Category>() { potatoCategory, tomatoCategory, eggsCategory });
+            Item chicken = new Item(currentBook, "Chicken", Description.CreateDescription("Chicken like in KFC"), new ObservableCollection<Category>() { chickenCategory, eggsCategory });
+        }
 
         #region StandartFunctions
         protected override void OnStart ()
--- NotABookWPF/MainWindow.xaml.cs
+++ NotABookWPF/MainWindow.xaml.cs
@@ -23,18 +23,64 @@ namespace NotABookWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Lists etc
+        public static Book currentBook = null;
+
+        public static ObservableCollection<Book> Books
+        {
+            get => Book.Books;
+            set => Book.Books = value;
+        }
+
+        public static ObservableCollection<Item> ItemsList => currentBook?.ItemsOfBook;
+        public static ObservableCollection<Category> CategoriesList => currentBook?.CategoriesOfBook;
+        public static ObservableCollection<CategoryInItem> CategoryInItemsList => currentBook?.CategoryInItemsOfBook;
+
+        #endregion
+
         public MainWindow()
         {
             InitializeComponent();
-            LeftListView.ItemsSource = new List<int>() { 10, 0, 2 };
-            CBStack.ItemsSource = new List<string>() { "first", "second", "third" };
-            Book book = new Book("This book");
-            ItemsListView.ItemsSource = new ObservableCollection<Item>() { new Item(book, "First"), new Item(book, "Second") };
+            SetUp();
+            LeftListView.ItemsSource = Books;            
+            ItemsListView.ItemsSource = ItemsList;
+        }
+
+        private void SetUp()
+        {
+            currentBook = new Book("My first book");
+            Book secondBook = new Book("Second book");
+            Book thirdBook = new Book("Third book");
+
+            Category chocolateCategory = new Category(currentBook, "Chocolate");
+            Category flourCategory = new Category(currentBook, "Flour");
+            Category eggsCategory = new Category(currentBook, "Eggs");
+            Category potatoCategory = new Category(currentBook, "Potato");
+            Category tomatoCategory = new Category(currentBook, "Tomato");
+            Category chickenCategory = new Category(currentBook, "Chicken");
+
+            Item chocolateBiscuit = new Item(currentBook, "Chocolate biscuit", Description.CreateDescription("The best chocolate cake ever"), new ObservableCollection<Category>() { chocolateCategory, flourCategory, eggsCategory });
+            Item salatWithPotatoAndTomato = new Item(currentBook, "Salat with potat, tomatos and eggs", Description.CreateDescription("Very healthy salat"), new ObservableCollection<Category>() { potatoCategory, tomatoCategory, eggsCategory });
+            Item chicken = new Item(currentBook, "Chicken", Description.CreateDescription("Chicken like in KFC"), new ObservableCollection<Category>() { chickenCategory, eggsCategory });
         }
 
         private void BtnText_Click(object sender, RoutedEventArgs e)
         {
             //BtnText.Content = ModelsLibrary.Class1.Method();
+        }
+
+        private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+        {            
+            this.Close();
+        }
+        private void MenuItemHello_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(((MenuItem)sender).Header.ToString());
+        }
+
+        private void ItemsListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            TBItem.Text = (ItemsListView.SelectedItem as Item).Title;
         }
     }
 }
