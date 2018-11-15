7cc0c7eadcd4c5f973578033949a26a7a60d0a35
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-11-15 16:42:03 +0200

    Created enum PtojectTypes

---
+++ NotABookLibraryStandart/Models/ProjectTypes.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotABookLibraryStandart.Models
+{
+    public enum ProjectTypes
+    {
+        Xamarin,
+        Wpf
+    }
+}
