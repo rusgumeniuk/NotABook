fc1b84386115eec074da5eab620ee1f4ed9eae53
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-12-08 05:29:35 +0200

    Create Note.cs

---
+++ NotABookLibraryStandart/Models/BookElements/Note.cs
@@ -0,0 +1,24 @@
+﻿using NotABookLibraryStandart.Models.BookElements.Contents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotABookLibraryStandart.Models.BookElements.Notes
+{
+    public class Note : Entity
+    {
+        private IList<IContent> contents = new List<IContent>();
+
+        public object GetContent()
+        {
+            List<object> list = new List<object>();
+            foreach (IContent item in contents)
+            {
+                list.Add(item.Content);
+
+            }
+            return list;
+        }
+
+    }
+}
