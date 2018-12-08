7e4807a13b5165fa6095f847239ad32bc1d5863b
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-12-08 06:23:12 +0200

    Added method IsEmptyContent(), GetTitleFromContent() and GetNextWord()

--- NotABookLibraryStandart/Models/BookElements/Contents/TextContent.cs
+++ NotABookLibraryStandart/Models/BookElements/Contents/TextContent.cs
@@ -23,5 +23,57 @@ namespace NotABookLibraryStandart.Models.BookElements.Contents
             clone.Content = this.content.Clone() as byte[];
             return clone;
         }
+
+        public override bool IsEmptyContent()
+        {
+            return String.IsNullOrWhiteSpace(content);
+        }
+
+        public override string GetTitleFromContent()
+        {
+            if (String.IsNullOrEmpty(content))
+                return null;
+
+            string title = content.Clone() as string;
+
+            if (content.Contains(" "))
+            {
+                StringBuilder sb = new StringBuilder();
+                for (byte countOfWords = 0; countOfWords < 4 && sb.Length < 30; ++countOfWords)
+                {
+                    string word = GetNextWord(ref title);
+                    if (word != null)
+                    {
+                        sb.Append(word + " ");
+                    }
+                    else
+                        break;
+                }
+
+                return (sb.Length < 30 ? sb.ToString() : sb.ToString().Substring(0, 29));
+
+            }
+            else
+                return GetNextWord(ref title);
+        }
+
+       
+
+        private string GetNextWord(ref string str)
+        {
+            if (String.IsNullOrWhiteSpace(str)) return null;
+
+            StringBuilder stringBuilder = new StringBuilder();
+            int i = 0;
+
+            for (; i < str.Length && str[i] != ' ' && stringBuilder.Length < 30; ++i)
+            {
+                stringBuilder.Append(str[i]);
+            }
+
+            str = str.Substring(str.Length > i + 1 ? i + 1 : i);
+
+            return stringBuilder.ToString();
+        }
     }
 }
