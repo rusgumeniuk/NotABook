061a3c09df18c9fb5e2867b8c75d3021f725620e
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-12-18 17:54:23 +0200

    Set up basic work like with Book note

--- NotABookLibraryStandart/Models/BookElements/Note.cs
+++ NotABookLibraryStandart/Models/BookElements/Note.cs
@@ -3,31 +3,166 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace NotABookLibraryStandart.Models.BookElements.Notes
+namespace NotABookLibraryStandart.Models.BookElements
 {
-    public class Note : Entity
+    public class Note : BookElement
     {
-        private IList<IContent> contents = new List<IContent>();
+        private IList<IContent> noteContents = new List<IContent>();
 
-        public object GetContent()
+        #region prop
+        public IList<IContent> Contents
         {
-            List<object> list = new List<object>();
-            foreach (IContent item in contents)
+            get => noteContents;
+            set
             {
-                list.Add(item.Content);
+                noteContents.Clear();
+                foreach (var content in value)
+                {
+                    noteContents.Add(content);
+                }
             }
-            return list;
+        }
+
+        public bool IsHasNotContent
+        {
+            get
+            {
+                foreach (var content in noteContents)
+                {
+                    if (!content.IsEmptyContent())
+                        return true;
+                }
+                return false;
+            }
+        }
+        public string TitleFromContent
+        {
+            get
+            {
+                foreach (var content in noteContents)
+                {
+                    if (content.GetTitleFromContent() != null)
+                        return content.GetTitleFromContent();
+                }
+                return null;
+            }
+        }
+        public TextContent FirstTextContent
+        {
+            get => noteContents[IndexOfTextContent] as TextContent;
+        }
+
+        public string SetTextToFirstTextContent
+        {
+            set
+            {
+                if (IndexOfTextContent != -1)
+                {
+                    (noteContents[IndexOfTextContent] as TextContent).Content = value;
+                }
+                else
+                    throw new ArgumentException("U shouldn't see this");
+            }
+        }
+
+        public int IndexOfTextContent
+        {
+            get
+            {
+                for (int i = 0; i < noteContents.Count; i++)               
+                {
+                    if (noteContents[i] is TextContent)
+                        return i;
+                }
+                return -1;
+            }
+        }
+
+        #endregion
+      
+        #region ctors
+        public Note(Book book) : base(book)
+        {
+            Contents.Add(new TextContent());
+        }
+
+        public Note(Book book, string title) : base(book, title)
+        {
+            Contents.Add(new TextContent());
+        }
+
+        public Note(Book book, string title, IList<IContent> contents) : this(book, title)
+        {
+            Contents = contents;
+        }
+
+        public Note(Book book, string title, IList<IContent> contents, IList<Category> categories) : this(book, title, contents)
+        {
+            SetCategories(book, categories);
+        }
+        #endregion
+
+        public void SetCategories(Book CurrentBook, IEnumerable<Category> categories)
+        {
+            if (IsNoteAndBookNotNull(CurrentBook, this))
+            {
+                if (categories != null)
+                {
+                    // CategoryInItem.DeleteAllConnectionWithItem(CurrentBook, this);
+
+                    foreach (var category in categories)
+                    {
+                        //CategoryInItem.CreateCategoryInItem(CurrentBook, category, this);
+                    }
+
+                    UpdateDateOfLastChanging();
+                }
+                else if (ProjectType != TypeOfRunningProject.Xamarin)
+                    throw new ArgumentNullException();
+            }
+        }
+
+        public IList<Category> GetCategories(Book CurrentBook)
+        {
+            if (Book.IsBookIsNotNull(CurrentBook) /*&& CategoryInItem.IsItemHasConnection(CurrentBook, this)*/)
+            {
+                IList<Category> categories = new List<Category>();
+                foreach (CategoryInItem pair in CurrentBook.CategoryInItemsOfBook)
+                {
+                    if (pair.GetItemId == Id)
+                    {
+                        categories.Add(pair.Category);
+                    }
+                }
+                return categories;
+            }
+            return null;
+        }
+
+
+        public static bool IsNoteAndBookNotNull(Book CurrentBook, Note note)
+        {
+            return IsNoteNotNull(note) && Book.IsBookIsNotNull(CurrentBook);
+        }
+
+        public static bool IsNoteNotNull(Note note)
+        {
+            return note != null ? true : (ProjectType == TypeOfRunningProject.Xamarin ? false : throw new ArgumentNullException());
         }
 
         public void AddContent(IContent content)
         {
-            contents.Add(content);
+            noteContents.Add(content);
         }
 
         public void RemoveContent(IContent content)
         {
-            contents.Remove(content);
+            noteContents.Remove(content);
         }
 
+        public bool IsContainContant(IContent content)
+        {
+            return noteContents.Contains(content);
+        }
     }
 }
