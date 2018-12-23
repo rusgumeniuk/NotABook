aa68ad72607d257f769ba10a89e490f1c23d1cce
rusgumeniuk <rus.gumeniuk@gmail.com>
2018-12-23 21:47:08 +0200

    Created LinkNoteCategory - class to find all links of some note with categories or category with notes

---
+++ NotABookLibraryStandart/Models/BookElements/LinkNoteCategory.cs
@@ -0,0 +1,24 @@
+﻿using NotABookLibraryStandart.Models.BookElements;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotABookLibraryStandart.Models
+{
+    public class LinkNoteCategory : Entity
+    {
+        public LinkNoteCategory() : base() { }
+        public LinkNoteCategory(Note note, Category category) : base()
+        {
+            Note = note ?? throw new ArgumentNullException(nameof(note));
+            Category = category ?? throw new ArgumentNullException(nameof(category));
+        }
+        public static LinkNoteCategory CreateConnection(Note note, Category category)
+        {
+            return new LinkNoteCategory(note, category);
+        }
+
+        public Note Note { get; set; }
+        public Category Category { get; set; }
+    }
+}
