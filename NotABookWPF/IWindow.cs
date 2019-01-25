d27a6033ba9dfe47b73b2ccd35c8196161e19480
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-01-25 22:31:12 +0200

    Create IWindow.cs

---
+++ NotABookWPF/IWindow.cs
@@ -0,0 +1,10 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace NotABookWPF
+{
+    interface IWindow
+    {
+        ViewModelBase ViewModel { get; set; }
+        void ProcessMessage(string message);
+    }
+}
