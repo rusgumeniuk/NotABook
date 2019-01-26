99bfbe46c5914abece7d9cd62bcb2198ce9315a9
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-01-26 17:00:15 +0200

    Added methods IsExistUser, AddUser, RemoveUser

--- NotABookLibraryStandart/Models/Roles/IAuthenticationService.cs
+++ NotABookLibraryStandart/Models/Roles/IAuthenticationService.cs
@@ -3,5 +3,9 @@
     public interface IAuthenticationService
     {
         User AuthenticateUser(string username, string password);
+        bool IsExistUser(string username, string password);
+        void AddUser(string username, string email, string hashedPassword, string[] roles);
+        bool RemoveUser(string username, string password);
+        bool RemoveUser(User user);
     }
 }
