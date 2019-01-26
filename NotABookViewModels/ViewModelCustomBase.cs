ef45d03f17b30656643567995c9878c79f3a77eb
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-01-26 17:01:32 +0200

    Create ViewModelCustomBase.cs

---
+++ NotABookViewModels/ViewModelCustomBase.cs
@@ -0,0 +1,15 @@
+﻿using GalaSoft.MvvmLight;
+
+using NotABookLibraryStandart.Models.Roles;
+
+namespace NotABookViewModels
+{
+    public class ViewModelCustomBase : ViewModelBase
+    {
+        protected readonly IAuthenticationService _authenticationService;
+        public ViewModelCustomBase(IAuthenticationService authenticationService)
+        {
+            _authenticationService = authenticationService;
+        }
+    }
+}
