15d893f7bf9f9b129ac1cfbbad322a76954002b8
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-01-27 21:38:48 +0200

    Added empty ctor and lists Books and Categories

--- NotABookLibraryStandart/Models/Roles/User.cs
+++ NotABookLibraryStandart/Models/Roles/User.cs
@@ -1,21 +1,47 @@
-﻿namespace NotABookLibraryStandart.Models.Roles
+﻿using NotABookLibraryStandart.Models.BookElements;
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace NotABookLibraryStandart.Models.Roles
 {
     public class User : Entity
     {
-        internal User(string username, string email, string[] roles)
+        public User()
+        {
+            Id = Guid.NewGuid();
+        }
+        public User(string username, string email, string[] roles) : this()
         {
             Username = username;
             Email = email;
             Roles = roles;
         }
-        internal User(string username, string email, string hashedPassword, string[] roles) : this(username, email, roles)
-        {           
+        public User(string username, string email, string hashedPassword, string[] roles) : this(username, email, roles)
+        {
             HashedPassword = hashedPassword;
+        }        
+
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string HashedPassword { get; set; }
+        public string[] Roles { get; set; }
+        public IList<Book> Books = new ObservableCollection<Book>();
+        public IList<Category> Categories = new ObservableCollection<Category>();
+
+        public static string CalculateHash(string clearTextPassword, string salt)
+        {
+            // Convert the salted password to a byte array
+            byte[] saltedHashBytes = Encoding.UTF8.GetBytes(clearTextPassword + salt);
+            // Use the hash algorithm to calculate the hash
+            HashAlgorithm algorithm = new SHA256Managed();
+            byte[] hash = algorithm.ComputeHash(saltedHashBytes);
+            // Return the hash as a base64 encoded string to be compared to the stored password
+            return Convert.ToBase64String(hash);
         }
-        public string Username { get; private set; }
-        public string Email { get; private set; }
-        public string HashedPassword { get; private set; }
-        public string[] Roles { get; private set; }
     }
 }
 
