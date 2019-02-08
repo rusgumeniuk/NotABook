f4e4ee9e728490e939b93326350b78ad0390fcc7
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-02-08 19:12:02 +0200

    Added prop User CurrentUser

--- NotABookViewModels/ViewModelCustomBase.cs
+++ NotABookViewModels/ViewModelCustomBase.cs
@@ -1,12 +1,17 @@
 ﻿using GalaSoft.MvvmLight;
 
 using NotABookLibraryStandart.DB;
+using NotABookLibraryStandart.Models.Roles;
 
 namespace NotABookViewModels
 {
     public class ViewModelCustomBase : ViewModelBase
     {
         public readonly IService Service;
+        public User CurrentUser
+        {
+            get => Service.GetUser((System.Threading.Thread.CurrentPrincipal as Principal).Identity.Name) ?? throw new System.UnauthorizedAccessException("Wrong CurrentUser");
+        }
         public ViewModelCustomBase(IService service)
         {
             Service = service;
