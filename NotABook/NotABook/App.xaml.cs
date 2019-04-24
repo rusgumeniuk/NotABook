cd34731441baef7844524c050291aa3b0077e836
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-04-24 11:28:57 +0300

    refactored App class (from old methods, lists etc)

--- NotABook/NotABook/App.xaml.cs
+++ NotABook/NotABook/App.xaml.cs
@@ -1,78 +1,43 @@
-using System;
-using System.Collections.Generic;
+using NotABookDataAccess;
+
+using NotABookLibraryStandart.DB;
+using NotABookLibraryStandart.Models;
+using NotABookLibraryStandart.Models.BookElements;
+using NotABookViewModels;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using System.Collections;
-using System.Linq;
-using System.Collections.ObjectModel;
 
-using NotABookLibraryStandart.Models;
-
-
-[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace NotABook
 {
-	public partial class App : Application
-	{
-        #region Lists etc
+    public partial class App : Application
+    {
         public static Book currentBook = null;
-
-        public static ObservableCollection<Book> Books
+        public App()
         {
-            get => Book.Books;
-            set => Book.Books = value;
-        }           
-       
-        public static ObservableCollection<Item> ItemsList => currentBook?.ItemsOfBook;
-        public static ObservableCollection<Category> CategoriesList => currentBook?.CategoriesOfBook;
-        public static ObservableCollection<CategoryInItem> CategoryInItemsList => currentBook?.CategoryInItemsOfBook;        
-
-        #endregion
-
-        public App ()
-		{
             Base.ProjectType = TypeOfRunningProject.Xamarin;
+            InitializeComponent();            
 
-            InitializeComponent();
-
-            SetUp();                     
-
-			MainPage = new Pages.MainPages.StartPage();                                  
-		}
-
-        private void SetUp()
-        {
-            currentBook = new Book("My first book");
-            Book secondBook = new Book("Second book");
-            Book thirdBook = new Book("Third book");
-
-            Category chocolateCategory = new Category(currentBook, "Chocolate");
-            Category flourCategory = new Category(currentBook, "Flour");
-            Category eggsCategory = new Category(currentBook, "Eggs");
-            Category potatoCategory = new Category(currentBook, "Potato");
-            Category tomatoCategory = new Category(currentBook, "Tomato");
-            Category chickenCategory = new Category(currentBook, "Chicken");
-
-            Item chocolateBiscuit = new Item(currentBook, "Chocolate biscuit", Description.CreateDescription(App.currentBook, "The best chocolate cake ever"), new ObservableCollection<Category>() { chocolateCategory, flourCategory, eggsCategory });
-            Item salatWithPotatoAndTomato = new Item(currentBook, "Salat with potat, tomatos and eggs", Description.CreateDescription(App.currentBook, "Very healthy salat"), new ObservableCollection<Category>() { potatoCategory, tomatoCategory, eggsCategory });
-            Item chicken = new Item(currentBook, "Chicken", Description.CreateDescription(App.currentBook, "Chicken like in KFC"), new ObservableCollection<Category>() { chickenCategory, eggsCategory });
+            LogInWindowViewModel viewModel = new LogInWindowViewModel(new Service(new Repository(new DataBaseContext())));
+            var window = new Pages.MainPages.LogInPage(viewModel);
         }
 
         #region StandartFunctions
-        protected override void OnStart ()
-		{                      
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
         #endregion
     }
 }
