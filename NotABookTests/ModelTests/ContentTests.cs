72be84ae879b143471f409203f8d7245c1353352
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-07-28 21:57:54 +0300

    Created UserTests and ContentTests classes with simple tests

---
+++ NotABookTests/ModelTests/ContentTests.cs
@@ -0,0 +1,28 @@
+﻿using NotABookLibraryStandart.Models.BookElements.Contents;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NotABookTests.ModelTests
+{
+    public class ContentTests
+    {
+        private readonly TextContent textContent;
+        private readonly PhotoContent photoContent;
+        public ContentTests()
+        {
+            textContent = new TextContent();
+            photoContent = new PhotoContent();
+        }
+
+        [Fact]
+        public void CreatePhotoContent_WhenSetWrongByteArray_ReturnsException()
+        {
+            PhotoContent content;
+            Action action = () => content = new PhotoContent() { Content = "tt" };
+            Assert.ThrowsAny<Exception>(action);
+
+        }
+    }
+}
---
+++ NotABookTests/ModelTests/UserTests.cs
@@ -0,0 +1,25 @@
+﻿using NotABookLibraryStandart.Models.Roles;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace NotABookTests.ModelTests
+{
+    public class UserTests
+    {
+        private readonly User user;
+        public UserTests()
+        {
+            user = new User();        
+        }
+
+        [Fact]
+        public void CreateUser_CreateDefaultBook_ReturnsNotEmpty()
+        {
+            User newUser = new User();
+            Assert.NotEmpty(newUser.Books);
+        }
+
+    }
+}
