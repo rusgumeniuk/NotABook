3ba480570e8126aa5d79eb289121e9220c7f4c6e
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-08-05 12:20:18 +0300

    Created empty AspNet Core app, adeed controller Home, view Index, layout. Published app to local machine.

---
+++ NotABook.WebAppCore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using NotABookLibraryStandart.Models.BookElements;
+
+namespace NotABook.WebAppCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {            
+            return View(new Book("First web book"));
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
