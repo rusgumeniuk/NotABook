e3076b6d7bf610111a0ef6e811e82c0849aa9937
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-08-10 14:18:18 +0300

    Created Signup- and LoginViewModel

---
+++ NotABook.WebAppCore/ViewModels/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotABook.WebAppCore.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Oh, u missed your username :c \r\n Input it please")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "We know that u have password, so u need to input it")]        
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ NotABook.WebAppCore/ViewModels/SignupViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotABook.WebAppCore.ViewModels
+{
+    public class SignupViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [StringLength(55, ErrorMessage = "Hmmm, we can't store your password because we even couldn't image that it can has this length. Try other length please.", MinimumLength = 6)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "No way... Your passwords have differences. \r\n But they should be same, so please try again")]
+        [Display(Name = "Confirm password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
