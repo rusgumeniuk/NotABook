42ce2e877c112159ab957a9ed8dafdd42bab58a2
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-08-12 10:06:49 +0300

    created index for NoteController and view model for this view

---
+++ NotABook.WebAppCore/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using NotABookLibraryStandart.Models.BookElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotABook.WebAppCore.ViewModels
+{
+    public class MainPageViewModel
+    {
+        public MainPageViewModel(Book currentBook, IEnumerable<Book> books, Note currentNote)
+        {
+            CurrentBook = currentBook;
+            Books = books;
+            CurrentNote = currentNote;
+        }
+
+        public Book CurrentBook { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+        public Note CurrentNote { get; set; }        
+    }
+}
