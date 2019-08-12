28e54486d63ae0a031d83b0f55a668b962ef106d
rusgumeniuk <rus.gumeniuk@gmail.com>
2019-08-12 10:07:35 +0300

    created NotePartial view and view model

---
+++ NotABook.WebAppCore/ViewModels/NotePartialViewModel.cs
@@ -0,0 +1,17 @@
+﻿using NotABookLibraryStandart.Models.BookElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NotABook.WebAppCore.ViewModels
+{
+    public class NotePartialViewModel
+    {
+        public Note Note { get; set; }
+        public IEnumerable<Category> AllCategories { get; set; }
+        public IEnumerable<Category> NoteCategories { get; set; }
+        public Book NoteBook { get; set; }
+        public IEnumerable<Book> AllBooks { get; set; }
+    }
+}
